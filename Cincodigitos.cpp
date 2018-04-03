820f57026284c74f7d3e23dca632ea8aad653e67
JersonUCSP <jerson.mamani@ucsp.edu.pe>
2018-04-02 22:26:39 -0500

    problema 7

---
+++ Cincodigitos.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Inserte un numero de 5 digitos"<<endl;
+	int num;
+	cin>>num;
+	if(num>=10000 and num<=99999){
+		for(int i=10000;i>=1;i/=10){
+			cout<<num/i<<"\t";
+			num=num%i;
+		}
+	}
+	else{
+		cout<<"El numero que has ingresado no tiene 5 digitos \n\n";
+		main();
+	}
+}
