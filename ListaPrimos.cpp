1fc4ce4f6b719e361912e2e1061da576116e406c
JersonUCSP <jerson.mamani@ucsp.edu.pe>
2018-04-02 22:25:49 -0500

    problema 6

---
+++ ListaPrimos.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int x,y,z;
+	cout<<"Inserte un numero ---> ";
+	cin>>x;
+	y=1;
+	z=2;
+	if (x==1 or x==0)
+		cout<<"No hay numero primos";
+	else{
+	while(z!=x){
+		for (int i=2;i<=z;i++){
+			if(z%i==0)
+				y+=1;}
+		
+		if(y<3){
+			cout<<z<<",";
+			z++;
+			y=1;
+		}
+		else{
+			z++;
+			y=1;
+		}
+	}
+}
+}
