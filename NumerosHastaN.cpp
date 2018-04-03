9fb91fd7a14ec886c1c3ab1dd6c199c51c0740ac
JersonUCSP <jerson.mamani@ucsp.edu.pe>
2018-04-02 22:21:34 -0500

    problema 2

---
+++ NumerosHastaN.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Ingrese un numero ---> ";
+    int a;
+    cin>>a;
+    for(int i=0;i<a;i++){
+    	cout<< i<<",";
+	}
+	cout<<a;
+}
