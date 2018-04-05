b79eefacc7ba6a4d6658d273c4aaeae15ffa67e8
ALUMNO-A <ALUMNO-A@UCSP.AULAB>
2018-04-05 09:08:17 -0500

    Anhadiendo operadores ternarios

--- Bisiesto.cpp
+++ Bisiesto.cpp
@@ -4,10 +4,7 @@ int main(){
       int a;
       cout<<"Ingresa anho"<<endl;
       cin>>a;
-     if(a % 4 == 0 && (a % 100 != 0 || a % 400 == 0)){
-         cout<<"El anho "<<a<<" si es bisiesto ";
-     }else{
-         cout<<"El anho "<<a<<" No es bisiesto ";
-     }
+     (a % 4 == 0 && (a % 100 != 0 || a % 400 == 0))? cout<<"El anho "<<a<<" si es bisiesto ":cout<<"El anho "<<a<<" No es bisiesto ";
+     
       
 }
