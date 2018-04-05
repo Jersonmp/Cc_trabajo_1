511f82907c95638efd2824448479627cadd82bef
ALUMNO-A <ALUMNO-A@UCSP.AULAB>
2018-04-05 09:49:40 -0500

    modificado

--- DosNumeros.cpp
+++ DosNumeros.cpp
@@ -7,19 +7,7 @@ int main(){
 	cout<<"inserte dos numeros "<<endl;
 	cin>>a;
 	cin>>b;
-	if (a%2==0 and b%2==0 ){
-		cout<<a<<" y "<<b<<" son multiplos de 2"<<endl;}
-	else
-		cout<<"uno de los dos numeros no es multiplo de 2";
-		
-	if(a%b==0)
-		cout<<a<<" es multiplo de "<<b<<endl;
-	else
-		cout<<a<<" no es multiplo de "<<b<<endl;
-	
-	if ((a*a)==b)
-		cout<<a<<" elevado al cuadrado es "<<b;
-	else
-		cout<<a<<" elevado al cuadrado no es "<<b;
-		
+	(a%2==0 and b%2==0 )?cout<<a<<" y "<<b<<" son multiplos de 2"<<endl:cout<<"uno de los dos numeros no es multiplo de 2";
+	(a%b==0)?cout<<a<<" es multiplo de "<<b<<endl:cout<<a<<" no es multiplo de "<<b<<endl;
+	((a*a)==b)?cout<<a<<" elevado al cuadrado es "<<b:cout<<a<<" elevado al cuadrado no es "<<b;		
 	}
