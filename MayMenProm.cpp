2fccfdac7afa24de0235e37d6078440f288c155e
JersonUCSP <jerson.mamani@ucsp.edu.pe>
2018-04-09 11:45:44 -0500

    Modificado O.T.

--- MayMenProm.cpp
+++ MayMenProm.cpp
@@ -5,38 +5,12 @@ using namespace std;
 int main(){
 	cout<<"Introduce 3 numeros"<<endl;
 	float a,b,c;
-	cin>>a;
-    cin>>b;
-    cin>>c;
+	cin>>a,cin>>b,cin>>c;
 
-    if (a>b){
-        if(a>c){
-            cout<<"el mayor es "<<a<<endl;
-            if(b>c)
-            cout<<"El menor es "<<c<<endl;
-            else
-            cout<<"El menor es "<<b<<endl;
-        }
-    }
-
-    if (b>a){
-        if (b>c){
-            cout<<"el mayor es "<<b<<endl;
-            if(a>c)
-            cout<<"El menor es "<<c<<endl;
-            else
-            cout<<"El menor es "<<a<<endl;
-    	}
-    }
-    if (c>a){
-        if (c>b){
-            cout<<"el mayor es "<<c<<endl;
-            if(a>b)
-            cout<<"El menor es "<<b<<endl;
-            else
-            cout<<"El menor es "<<a<<endl;
-    	}
-    }
+    (a>b)?((a>c)?cout<<"el mayor es "<<a<<endl,((b>c)?cout<<"El menor es "<<c<<endl:cout<<"El menor es "<<b<<endl):true):true;
+    (b>a)?((b>c)?cout<<"el mayor es "<<b<<endl,((a>c)?cout<<"El menor es "<<c<<endl:cout<<"El menor es "<<a<<endl):true):true;
+    (c>a)?((c>b)?cout<<"el mayor es "<<c<<endl,((a>b)?cout<<"El menor es "<<b<<endl:cout<<"El menor es "<<a<<endl):true):true;
+    	
     cout<<"El promedio de los 3 numeros es "<<(a+b+c)/3;
     
 }
