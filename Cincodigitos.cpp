6a9e94c346d755be80b057d5ade020de2a9876f5
JersonUCSP <jerson.mamani@ucsp.edu.pe>
2018-04-09 23:40:25 -0500

    Modificado con funcion

--- Cincodigitos.cpp
+++ Cincodigitos.cpp
@@ -1,18 +1,21 @@
 #include<iostream>
 using namespace std;
-int main(){
-	cout<<"Inserte un numero de 5 digitos"<<endl;
-	int num;
-	cin>>num;
-	if(num>=10000 and num<=99999){
+void ImprimeSeparados(int numero){
+	if(numero>=10000 and numero<=99999){
 		for(int i=10000;i>=1;i/=10)
 		{			
-			cout<<num/i<<"\t";
-			num=num%i;
+			cout<<numero/i<<"\t";
+			numero=numero%i;
 		}
 	}
 	else{
 		cout<<"El numero que has ingresado no tiene 5 digitos \n\n";
-		main();
 	}
+}
+int main(){
+	cout<<"Inserte un numero de 5 digitos"<<endl;
+	int num;
+	cin>>num;
+	ImprimeSeparados(num);
+	
 }
