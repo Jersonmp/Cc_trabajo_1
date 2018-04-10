dae01a213832a516b468d8fc322d15f9b370eff6
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2018-04-10 11:11:57 -0500

    Modificado

--- EsPrimoConDivisible.cpp
+++ EsPrimoConDivisible.cpp
@@ -1,19 +1,18 @@
+
 #include<iostream>
 using namespace std;
-void EsPrimo(int x){
+bool EsPrimo(int x){
 	int y,z;
-	
-	(x==1 or x==0)?cout<<"No es primo":true;                                       
+	y=1;
+	if(x==1 or x==0)
+		return false;
 	if(x>=2){
-	for(int i=1;i<x;i++)
-		(x%i==0)?(y+=1,(y==2)?z=i:true):true;
-		//(x%i==0)?y+=1:true;
-	(y<3)?cout<<x<<" es primo ":cout<<x<<" no es primo, su primer divisible es --> "<<z;
+		for(int i=2;i<x;i++)
+			(x%i==0)?y+=1:true;
+		return (y<3)?true:false;
 	}
 }
 int main(){
-	cout<<"Inserte numero -->  ";
-	int num;
-	cin>>num;
-	EsPrimo(num);
+	
+	cout<<EsPrimo(7);
 }
