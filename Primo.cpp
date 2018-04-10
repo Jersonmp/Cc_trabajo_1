2a0c043b14b3913eb7833a41f7df459a89aa70d7
JersonUCSP <jerson.mamani@ucsp.edu.pe>
2018-04-09 23:00:52 -0500

    Modificado O.T.

--- Primo.cpp
+++ Primo.cpp
@@ -1,20 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int x,y;
+	int x,y,z;
 	cout<<"Inserte un numero ---> ";
 	cin>>x;
 	y=1;
-	if (x==1 or x==0)
-		cout<<"No es primo";
-	else {
-		for (int i=2;i<x;i++){
-			if(x%i==0)
-				y+=1;}
-		
-		if(y<3)
-			cout<<x<<" es primo ";
-		else
-			cout<<x<<" no es primo";
+	(x==1 or x==0)?cout<<"No es primo":true;                                       
+	if(x>=2){
+	for(int i=2;i<x;i++)(x%i==0)?y+=1:true;
+		(y<3)?cout<<x<<" es primo ":cout<<x<<" no es primo\n";
 	}
 }
+
