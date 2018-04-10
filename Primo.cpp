40c18638a5733f62aa11170f85d0bf468ec5a2da
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2018-04-10 10:49:26 -0500

    Modificado

--- Primo.cpp
+++ Primo.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 using namespace std;
-void EsPrimo(int x){
+bool EsPrimo(int x){
 	int y,z;
 	y=1;
-	(x==1 or x==0)?cout<<"No es primo":true;                                       
+	if(x==1 or x==0)
+		return false;
 	if(x>=2){
-	for(int i=2;i<x;i++)(x%i==0)?y+=1:true;
-		(y<3)?cout<<x<<" es primo ":cout<<x<<" no es primo\n";
+	for(int i=2;i<x;i++)
+		(x%i==0)?y+=1:true;
+		return (y<3)?true:false;
 	}
 }
 int main(){
-	cout<<"Inserte numero -->";
-	int num;
-	cin>>num;
-	EsPrimo(num);
+	
+	cout<<EsPrimo(7);
 }
