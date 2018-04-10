2f46d7e97e39b05e7b2e76af302b5a87c7da901e
JersonUCSP <jerson.mamani@ucsp.edu.pe>
2018-04-10 00:06:23 -0500

    Modificado O.T. y funcion

--- MayorOMenor.cpp
+++ MayorOMenor.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 
 using namespace std;
+int main();
+
+void MayMenEdad(int x){
+	(x<18 and x>=0)?cout<<"Eres menor de edad "<<endl:true;
+    (x>18)?cout<<"Eres mayor de edad":true;
+    (x<0)?cout<<"Inserta un numero positivo\n",main():true;
+}
 
 int main()
 {
     cout << "Introduce tu edad ---> ";
-    int x;
-    cin>>x;
-    (x<18 and x>=0)?cout<<"Eres menor de edad "<<endl:true;
-    (x>18)?cout<<"Eres mayor de edad":true;
-    (x<0)?cout<<"Inserta un numero positivo\n",main():true;
+    int numero;
+    cin>>numero;
+    MayMenEdad(numero);
+    
 }
