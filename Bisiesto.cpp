c13c7b8299a413e43c9998d26e05aed29eeedea1
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2018-04-10 10:17:57 -0500

    Finalizado

--- Bisiesto.cpp
+++ Bisiesto.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 using namespace std;
 
-void bisiesto(int num){
-	(num % 4 == 0 && (num % 100 != 0 || num % 400 == 0))? cout<<"El anho "<<num<<" si es bisiesto ":cout<<"El anho "<<num<<" No es bisiesto ";
+bool bisiesto(int num){
+	return (num % 4 == 0 && (num % 100 != 0 || num % 400 == 0))? true :false;
 }
 
 int main(){
-	int anho;
-	cout<<"Inserte el anho--> ";
-	cin>>anho;
-	bisiesto(anho);
+	cout<<bisiesto(1800);
 }
