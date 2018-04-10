d0abf437ea3b5cd9f4d7a6a7cc60948530b23b63
JersonUCSP <jerson.mamani@ucsp.edu.pe>
2018-04-09 23:58:52 -0500

    Modificado O.T. y funcion

--- ListaPrimos.cpp
+++ ListaPrimos.cpp
@@ -1,15 +1,14 @@
 #include<iostream>
 using namespace std;
-int main(){
-	int x,y,z;
-	cout<<"Inserte un numero ---> ";
-	cin>>x;
+//Imprime los primos hasta N
+void PrimosHastaN(int num){
+	int y,z;
 	y=1;
 	z=2;
-	if (x==1 or x==0)
+	if (num==1 or num==0)
 		cout<<"No hay numero primos";
 	else{
-	while(z!=x){
+	while(z!=num){
 		for (int i=2;i<=z;i++){
 			if(z%i==0)
 				y+=1;}
@@ -17,6 +16,12 @@ int main(){
 		(y<3)?cout<<z<<"\n",z++,y=1:z++,y=1;
 		}
 	}
+	
+}
+int main(){
+	cout<<"Inserta un numero --> ";
+	int x;
+	cin>>x;
+	PrimosHastaN(x);
 	}
-
 
