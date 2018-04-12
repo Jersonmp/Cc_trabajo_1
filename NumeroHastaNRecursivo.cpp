ca3b1b2b9a47ad82a52640c389bc39ab1897586f
ALUMNO-A <ALUMNO-A@UCSP.AULAB>
2018-04-12 09:59:38 -0500

    falta terminar

---
+++ NumeroHastaNRecursivo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include"Primo.cpp"
+using namespace std;
+int mayor
+void PrimosHastaN(int x, int c=0,int i=0){
+	if(c==x){
+		return;
+
+	}
+	if (EsPrimo(i){
+		cout<<i<<endl;
+		PrimosHastaN(x;++c;++i);}
+	
+	else{
+		PrimosHastaN(x;c;i++)
+		}
+}
+
+int main(){
+	cout<<"ingresa numero"<<endl;
+	int
+}
+
+
