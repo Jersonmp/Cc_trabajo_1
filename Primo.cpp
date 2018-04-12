898245596e67e7020b0c387d3e1ef2fd0095adec
ALUMNO-A <ALUMNO-A@UCSP.AULAB>
2018-04-12 09:34:55 -0500

    Funcion primo

--- Primo.cpp
+++ Primo.cpp
@@ -1,17 +1,11 @@
 #include<iostream>
 using namespace std;
 bool EsPrimo(int x){
-	int y,z;
-	y=1;
-	if(x==1 or x==0)
-		return false;
-	if(x>=2){
-	for(int i=2;i<x;i++)
-		(x%i==0)?y+=1:true;
-		return (y<3)?true:false;
+
+	for(int i=2;i<x;i++){
+		if(!(x%i))
+			return false;
+		
 	}
-}
-int main(){
-	
-	cout<<EsPrimo(7);
+	return true;
 }
